324fd0b4b25028502910329d7d831b2b80063c96
poco <godiva140@gmail.com>
2023-11-29 19:01:00 +0900

    Section 1 완료

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,35 @@
-import { useState } from "react";
-
-import { PostDetail } from "./PostDetail";
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
+// 만약 useQuery가 반환하는 data에 다음 페이지 유무에 대한 값이 있다면 그걸 사용하면 된다
 
-async function fetchPosts() {
+// 현재 currentPage를 무한으로 호출하는 버그 존재
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['posts', currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000,
+  });
+
+  if (isLoading) return <h3>Loading...</h3>;
+  if (isError)
+    return (
+      <>
+        <h3>Error!</h3>
+        <p>{error.message}</p>
+      </>
+    );
 
   return (
     <>
@@ -31,11 +45,17 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={setSelectedPost((prev) => prev - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={setCurrentPage((prev) => prev + 1)}
+        >
           Next page
         </button>
       </div>
